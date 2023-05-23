55e8c85aad730f38a1963d8b5b8d458e984ae21d
Diego Soares <diegocosamores@gmail.com>
2023-05-23 02:50:20 -0300

    tested and fully functional

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from google.cloud import bigquery
 
 # GCP Credential
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/diego/OneDrive/Área de Trabalho/PDI/Secret/ifsp-pdi-d3cb64044d21.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "caminho para a chave GCP.json"
 
 
 # Schema Builder
